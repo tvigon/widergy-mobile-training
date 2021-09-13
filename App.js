c94579dedf43a058bc3bfc75053d43f8bd7ef063
tvigon <tomas.vigon@widergy.com>
2021-09-13 17:55:13 -0300

    mas prolijo, faltan funciones DEL y =

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -15,34 +16,31 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {Button, SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 
-const getScreenButtons = (setVal, opFunc) => [
-  {label: '7', onPress: setVal},
-  {label: '8', onPress: setVal},
-  {label: '9', onPress: setVal},
-  {label: '4', onPress: setVal},
-  {label: '5', onPress: setVal},
-  {label: '6', onPress: setVal},
-  {label: '1', onPress: setVal},
-  {label: '2', onPress: setVal},
-  {label: '3', onPress: setVal},
-  {label: '0', onPress: setVal},
-  {label: '.', onPress: setVal},
-  {label: 'DEL', onPress: opFunc},
-  {label: '+', onPress: setVal},
-  {label: '-', onPress: setVal},
-  {label: 'x', onPress: setVal},
-  {label: '/', onPress: setVal},
-  {label: '=', onPress: opFunc},
+const getScreenButtons = setVal => [
+  {label: '7', onPress: () => {setVal(prevValue => prevValue + '7');}},
+  {label: '8', onPress: () => {setVal(prevValue => prevValue + '8');}},
+  {label: '9', onPress: () => {setVal(prevValue => prevValue + '9');}},
+  {label: '4', onPress: () => {setVal(prevValue => prevValue + '4');}},
+  {label: '5', onPress: () => {setVal(prevValue => prevValue + '5');}},
+  {label: '6', onPress: () => {setVal(prevValue => prevValue + '6');}},
+  {label: '1', onPress: () => {setVal(prevValue => prevValue + '1');}},
+  {label: '2', onPress: () => {setVal(prevValue => prevValue + '2');}},
+  {label: '3', onPress: () => {setVal(prevValue => prevValue + '3');}},
+  {label: '0', onPress: () => {setVal(prevValue => prevValue + '0');}},
+  {label: '.', onPress: () => {setVal(prevValue => prevValue + '.');}},
+  {label: '+', onPress: () => {setVal(prevValue => prevValue + '+');}},
+  {label: '-', onPress: () => {setVal(prevValue => prevValue + '-');}},
+  {label: 'x', onPress: () => {setVal(prevValue => prevValue + 'x');}},
+  {label: '/', onPress: () => {setVal(prevValue => prevValue + '/');}},
 ];
 
-const renderButtons = button => (
-  <TouchableOpacity onPress={button.onPress} style={[styles.buttonStyle]}>
-    <Text style={[styles.text]}>{button.label}</Text>
-  </TouchableOpacity>
-);
+const getOpScreenButtons = futureFunc => [
+  {label: 'DEL', onPress: futureFunc},
+  {label: '=', onPress: futureFunc},
+];
 
-const renderOpButtons = button => (
-  <TouchableOpacity onPress={button.onPress} style={[styles.opsStyle]}>
+const renderButtons = (button, useNumericalStyle ) => (
+  <TouchableOpacity onPress={button.onPress} style={useNumericalStyle ? styles.buttonStyle : styles.opsStyle}>
     <Text style={[styles.text]}>{button.label}</Text>
   </TouchableOpacity>
 );
@@ -53,7 +51,7 @@ const App = () => {
   const [error, setError] = useState('');
 
   const SCREEN_BUTTONS = getScreenButtons(setValue);
-
+  const OP_SCREEN_BUTTONS = getOpScreenButtons();
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.screen}>
@@ -65,10 +63,12 @@ const App = () => {
       </View>
       <View style={[styles.row, styles.allButtons]}>
         <View style={[styles.row, styles.numButtons]}>
-          {SCREEN_BUTTONS.splice(0, 12).map(renderButtons)}
+          {SCREEN_BUTTONS.splice(0, 11).map(button => renderButtons(button, true))}
+          {OP_SCREEN_BUTTONS.splice(0,1).map(button => renderButtons(button,true))}
         </View>
         <View style={[styles.opsStyle]}>
-          {SCREEN_BUTTONS.map(renderOpButtons)}
+          {SCREEN_BUTTONS.splice(0, 4).map(button => renderButtons(button, false))}
+          {OP_SCREEN_BUTTONS.splice(0,1).map(button => renderButtons(button,false))}
         </View>
       </View>
     </SafeAreaView>
